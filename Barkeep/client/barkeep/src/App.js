3fd1f85d99721dd8524757f49a4ed7c9c05f4c94
Logan Van Meter <vanmeterlj@gmail.com>
2024-02-22 20:15:18 -0500

    added fetch calls for user

--- Barkeep/client/barkeep/src/App.js
+++ Barkeep/client/barkeep/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Router } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [isSiteAdminLoggedIn, setIsSiteAdminLoggedIn] = useState(true);
+	useEffect(() => {
+		if (!localStorage.getItem("siteAdmin")) {
+			setIsSiteAdminLoggedIn(false);
+		}
+	}, [isSiteAdminLoggedIn]);
 
-export default App;
+	if (isSiteAdminLoggedIn) {
+		return <Router></Router>;
+	}
+};
